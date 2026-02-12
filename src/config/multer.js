771776673caa08f3fd59a536f8401ee90c21018d
Prabhatkrdprr24 <prabhatkrdprr24@gmail.com>
2026-02-12 11:28:22 +0530

    file check in multer before upload code added

--- src/config/multer.js
+++ src/config/multer.js
@@ -7,7 +7,18 @@ const storage = multerS3({
     s3: s3,
     bucket: AWS_BUCKET_NAME,
     key: function (req, file, cb) {
-        console.log(file);
+        console.log("file in multer print: ", file);
+        if(!file){
+            console.log("No file received in multer");
+            return cb(new Error("No file received"), null);
+        }
+
+        // check for file mimetype i.e only jpeg and png files are allowed
+        if(file.mimetype !== "image/jpeg" && file.mimetype !== "image/png"){
+            console.log("Invalid file type received in multer: ", file.mimetype);
+            return cb(new Error("Invalid file type. Only JPEG and PNG are allowed."), null);
+        }
+        
         const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1E9);
         cb(null, file.fieldname + "-" + uniqueSuffix + "." + file.mimetype.split("/")[1]);
     }
